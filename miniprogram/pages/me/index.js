cecd38a63c0abb6295e73c21584e11340151ec78
jeo-zju <jeo.zju@gmail.com>
2026-09-02 23:39:56 +0800

    chore: 版本号 2.0.17→2.0.22（6 issue：#23 #24 #25 #26 #27 #28）

--- miniprogram/pages/me/index.js
+++ miniprogram/pages/me/index.js
@@ -53,7 +53,7 @@ Page({
     defaultAvatar: DEFAULT_AVATAR,
     canSeeToolbox: false,
     drawerOpen: false,
-    version: "2.0.17",
+    version: "2.0.22",
 
     credit: { remaining: 0, limit: 10 },
     creditPercent: 0,
